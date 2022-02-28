650c00e6ca3514a3bd9d476cf6b7b3f698c2f1be
Tran Quang <kysumuoiba@teneocto.io>
2022-02-28 17:05:46 +0700

    update code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { message } from './controllers/socket';
-const { instrument } = require('@socket.io/admin-ui');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { message, notification } from "./controllers/socket";
+const { instrument } = require("@socket.io/admin-ui");
 
 const app = express();
 dotenv.config();
@@ -23,11 +23,11 @@ dotenv.config();
 // });
 
 // server with socket
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   },
 });
 instrument(io, {
@@ -35,19 +35,19 @@ instrument(io, {
 });
 
 // Router
-const authRouters = require('./routes/auth');
-const serviceRouter = require('./routes/service');
-const positionRouter = require('./routes/position');
-const staffRouter = require('./routes/staff');
-const categoryRouter = require('./routes/category');
-const productRouter = require('./routes/product');
-const salaryRouter = require('./routes/salary');
-const userRouter = require('./routes/user');
-const appointmentRouter = require('./routes/appointment');
-const customerRouter = require('./routes/customer');
-const invoiceRouter = require('./routes/invoice');
-const detailInvoiceRouter = require('./routes/detailInvoice');
-const employeeJobDetail = require('./routes/employeeJobDetail');
+const authRouters = require("./routes/auth");
+const serviceRouter = require("./routes/service");
+const positionRouter = require("./routes/position");
+const staffRouter = require("./routes/staff");
+const categoryRouter = require("./routes/category");
+const productRouter = require("./routes/product");
+const salaryRouter = require("./routes/salary");
+const userRouter = require("./routes/user");
+const appointmentRouter = require("./routes/appointment");
+const customerRouter = require("./routes/customer");
+const invoiceRouter = require("./routes/invoice");
+const detailInvoiceRouter = require("./routes/detailInvoice");
+const employeeJobDetail = require("./routes/employeeJobDetail");
 
 //db connection
 mongoose
@@ -55,9 +55,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB Connected'));
+  .then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.log(`DB connection error: ${err.message}`);
 });
 
@@ -65,26 +65,27 @@ mongoose.connection.on('error', (err) => {
 app.use(express.json());
 app.use(
   cors({
-    credentials: 'same-origin',
+    credentials: "same-origin",
   })
 );
 
 message(io);
+notification(io);
 
-app.use('/api', authRouters);
-app.use('/api', serviceRouter);
-app.use('/api', positionRouter);
-app.use('/api', staffRouter);
-app.use('/api', categoryRouter);
-app.use('/api', productRouter);
-app.use('/api', salaryRouter);
-app.use('/api', userRouter);
+app.use("/api", authRouters);
+app.use("/api", serviceRouter);
+app.use("/api", positionRouter);
+app.use("/api", staffRouter);
+app.use("/api", categoryRouter);
+app.use("/api", productRouter);
+app.use("/api", salaryRouter);
+app.use("/api", userRouter);
 // api đặt lịch
-app.use('/api', appointmentRouter);
-app.use('/api', customerRouter);
-app.use('/api', invoiceRouter);
-app.use('/api', detailInvoiceRouter);
-app.use('/api', employeeJobDetail);
+app.use("/api", appointmentRouter);
+app.use("/api", customerRouter);
+app.use("/api", invoiceRouter);
+app.use("/api", detailInvoiceRouter);
+app.use("/api", employeeJobDetail);
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
