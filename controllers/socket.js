54ef76e3976bfb76d72f74c1d640ca78a0eff360
Trần Quang <songoku2192@gmail.com>
2022-03-13 06:17:46 +0700

    ud

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,41 +1,41 @@
-import EmployeeJobDetail from '../models/employeeJobDetailModel';
-import {
-  createNotificationStaff,
-  listNotificationStaff,
-} from './notificationStaffController';
+// import EmployeeJobDetail from '../models/employeeJobDetailModel';
+// import {
+//   createNotificationStaff,
+//   listNotificationStaff,
+// } from './notificationStaffController';
 
-function employeeJobDetail(id) {
-  return new Promise((resolve, reject) => {
-    EmployeeJobDetail.find({ _id: id })
-      .populate([
-        { path: 'service_id' },
-        { path: 'staff_id', populate: { path: 'user_id' } },
-      ])
-      .exec((err, data) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(data);
-      });
-  });
-}
+// function employeeJobDetail(id) {
+//   return new Promise((resolve, reject) => {
+//     EmployeeJobDetail.find({ _id: id })
+//       .populate([
+//         { path: 'service_id' },
+//         { path: 'staff_id', populate: { path: 'user_id' } },
+//       ])
+//       .exec((err, data) => {
+//         if (err) {
+//           reject(err);
+//         }
+//         resolve(data);
+//       });
+//   });
+// }
 
 export const notification = (io) => {
-  io.on('connection', (socket) => {
-    socket.on('disconnect', () => {});
-    socket.on('notifications', (data) => {
-      employeeJobDetail(data).then((noti) => {
-        socket.emit('send-message', noti);
-      });
-    });
-    socket.on('notifications-staff', (id) => {
-      createNotificationStaff(id).then((data) => {
-        listNotificationStaff(data.staff_id).then((noti) => {
-          socket.broadcast.emit('send-message', noti);
-        });
-      });
-    });
-  });
+  // io.on('connection', (socket) => {
+  //   socket.on('disconnect', () => {});
+  //   socket.on('notifications', (data) => {
+  //     employeeJobDetail(data).then((noti) => {
+  //       socket.emit('send-message', noti);
+  //     });
+  //   });
+  //   socket.on('notifications-staff', (id) => {
+  //     createNotificationStaff(id).then((data) => {
+  //       listNotificationStaff(data.staff_id).then((noti) => {
+  //         socket.broadcast.emit('send-message', noti);
+  //       });
+  //     });
+  //   });
+  // });
 };
 
 // export const message = (io) => {
