5f180c539479587d389a3f743e0d707d9d25b169
Trần Quang <songoku2192@gmail.com>
2022-03-12 14:48:55 +0700

    push code update

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema(
     email: {
       type: String,
       trim: true,
-      default: '',
+      default: '' + uuidv4(),
     },
     uid: {
       required: true,
