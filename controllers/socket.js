dcd47427e261e9d128f3b788e346f670acdeed7d
Trần Quang <songoku2192@gmail.com>
2022-03-13 06:42:37 +0700

    update code fix bug

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,55 +1,55 @@
-import NotificationStaff from '../models/NotificationStaffModel';
+import NotificationStaff from '../models/notificationStaffModel';
 
-// const listNotificationStaff = (staffId) => {
-//   const ObjectId = require('mongodb').ObjectId;
-//   const id = new ObjectId(staffId);
-//   return new Promise((resolve, reject) => {
-//     NotificationStaff.find({ staff_id: id })
-//       .populate([{ path: 'staff_id' }, { path: 'appointments_id' }])
-//       .exec((err, data) => {
-//         if (err) {
-//           reject(err);
-//         }
-//         resolve(data);
-//       });
-//   });
-// };
+const listNotificationStaff = (staffId) => {
+  const ObjectId = require('mongodb').ObjectId;
+  const id = new ObjectId(staffId);
+  return new Promise((resolve, reject) => {
+    NotificationStaff.find({ staff_id: id })
+      .populate([{ path: 'staff_id' }, { path: 'appointments_id' }])
+      .exec((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+  });
+};
 
-// const createNotificationStaff = (data) => {
-//   const notificationStaff = new NotificationStaff(data);
-//   return new Promise((resolve, reject) => {
-//     notificationStaff.save((err, data) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(data);
-//     });
-//   });
-// };
+const createNotificationStaff = (data) => {
+  const notificationStaff = new NotificationStaff(data);
+  return new Promise((resolve, reject) => {
+    notificationStaff.save((err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
-// const removeNotificationStaff = (idNoti) => {
-//   return new Promise((resolve, reject) => {
-//     NotificationStaff.find({ _id: idNoti })
-//       .remove()
-//       .exec((err) => {
-//         if (err) {
-//           reject(err);
-//         }
-//         resolve('remove success');
-//       });
-//   });
-// };
+const removeNotificationStaff = (idNoti) => {
+  return new Promise((resolve, reject) => {
+    NotificationStaff.find({ _id: idNoti })
+      .remove()
+      .exec((err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve('remove success');
+      });
+  });
+};
 
 export const notification = (io) => {
   io.on('connection', (socket) => {
     socket.on('disconnect', () => {});
-    // socket.on('notifications-staff', (id) => {
-    //   createNotificationStaff(id).then((data) => {
-    //     listNotificationStaff(data.staff_id).then((noti) => {
-    //       socket.broadcast.emit('send-message', noti);
-    //     });
-    //   });
-    // });
+    socket.on('notifications-staff', (id) => {
+      createNotificationStaff(id).then((data) => {
+        listNotificationStaff(data.staff_id).then((noti) => {
+          socket.broadcast.emit('send-message', noti);
+        });
+      });
+    });
   });
 };
 
