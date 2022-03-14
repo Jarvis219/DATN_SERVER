0157589b865e7263db279e867701bed89691dea9
Hồ Hoàng Phi <phihph>
2022-03-14 11:25:30 +0700

    update api/cart

--- models/serviceModel.js
+++ models/serviceModel.js
@@ -1,53 +1,51 @@
 import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Schema;
-const ServiceSchema = new mongoose.Schema(
-  {
+const ServiceSchema = new mongoose.Schema({
     service_name: {
-      type: String,
-      required: true,
-      maxLength: 100,
+        type: String,
+        required: true,
+        maxLength: 100,
     },
     service_price: {
-      type: Number,
-      required: true,
-      maxLength: 21,
+        type: Number,
+        required: true,
+        maxLength: 21,
     },
     service_sale: {
-      type: Number,
-      required: true,
-      default: 0,
-      maxLength: 21,
+        type: Number,
+        required: true,
+        default: 0,
+        maxLength: 21,
     },
     service_star: {
-      type: Number,
-      required: true,
-      default: 0,
-      maxLength: 5,
+        type: Number,
+        required: true,
+        default: 0,
+        maxLength: 5,
     },
     service_description: {
-      type: String,
-      required: true,
-      maxLength: 300,
+        type: String,
+        required: true,
+        maxLength: 300,
     },
     service_content: {
-      type: String,
+        type: String,
     },
     service_album: {
-      type: Array,
+        type: Array,
+        required: true,
     },
     category_id: {
-      type: ObjectId,
-      ref: 'Category',
-      required: true,
+        type: ObjectId,
+        ref: 'Category',
+        required: true,
     },
     service_time: {
-      type: Number,
-      required: true,
+        type: Number,
+        required: true,
     },
-  },
-  {
+}, {
     timestamps: true,
-  }
-);
+});
 
 module.exports = mongoose.model('Service', ServiceSchema);
---
+++ routes/cart.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+    addCart,
+    listCart,
+    listCartUser,
+    cartByID,
+    removeCart,
+    readCart,
+    updateCart,
+    removeAllItem
+} from "../controllers/cartControllers";
+import { userID } from "../controllers/userControllers";
+// import { isAuth } from "../controllers/auth";
+const router = express.Router();
+
+router.get("/list-cart", listCart);
+router.get("/list-cart/user", listCartUser);
+router.get("/read-cart/:id", readCart);
+router.post("/create-cart", addCart);
+router.put("/update-cart/:id", updateCart);
+router.delete("/remove-cart/:id", removeCart);
+router.put("/remove-all-item/:id", removeAllItem);
+
+router.param("id", cartByID);
+// router.param("userId", userID);
+module.exports = router;
