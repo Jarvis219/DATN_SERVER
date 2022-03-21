7778eb0b792a096e1a942b91b9ec716e5b71d37b
Tran Quang <kysumuoiba@teneocto.io>
2022-03-21 14:23:28 +0700

    update ctl

--- controllers/appointmentControllers.js
+++ controllers/appointmentControllers.js
@@ -1,49 +1,54 @@
-import Appointment from '../models/appointmentModel';
-import _ from 'lodash';
+import Appointment from "../models/appointmentModel";
+import _ from "lodash";
 
 export const listAppointment = (req, res) => {
   Appointment.find()
     .populate([
-      { path: 'service_id' },
-      { path: 'customer_id' },
-      { path: 'staff_id', populate: { path: 'user_id' } },
+      { path: "service_id" },
+      { path: "customer_id" },
+      { path: "staff_id", populate: { path: "user_id" } },
     ])
     .exec((err, data) => {
       if (err) {
         return res.status(500).json({
-          error: 'Appointment not found!',
+          error: "Appointment not found!",
         });
       }
       res.status(200).json({ data });
     });
 };
 
-export const createAppointment = (req, res) => {
-  const appointment = new Appointment(req.body);
-  appointment.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Add Appointment failed!',
-      });
-    }
-    res.json({
+export const createAppointment = async (req, res) => {
+  const appointment = await new Appointment(req.body);
+  try {
+    const currentAppointment = await appointment.save();
+    const data = await currentAppointment.populate([
+      { path: "service_id" },
+      { path: "customer_id" },
+      { path: "staff_id", populate: { path: "user_id" } },
+    ]);
+    return res.json({
       data,
-      message: 'Create Appointment successfully',
+      message: "Create Appointment successfully",
     });
-  });
+  } catch (error) {
+    return res.status(400).json({
+      error: "Add Appointment failed!",
+    });
+  }
 };
 
 export const appointmentId = (req, res, next, id) => {
   Appointment.findById(id)
     .populate([
-      { path: 'service_id' },
-      { path: 'customer_id' },
-      { path: 'staff_id', populate: { path: 'user_id' } },
+      { path: "service_id" },
+      { path: "customer_id" },
+      { path: "staff_id", populate: { path: "user_id" } },
     ])
     .exec((err, data) => {
       if (err) {
         return res.status(404).json({
-          error: 'Appointment not found!',
+          error: "Appointment not found!",
         });
       }
       req.appointment = data;
@@ -60,30 +65,35 @@ export const removeAppointment = (req, res) => {
   appointment.remove((err, data) => {
     if (err) {
       return res.status(400).json({
-        error: 'Delete appointment failed!',
+        error: "Delete appointment failed!",
       });
     }
     res.json({
-      message: 'Delete appointment successfully',
+      message: "Delete appointment successfully",
       data,
     });
   });
 };
 
-export const updateAppointment = (req, res) => {
+export const updateAppointment = async (req, res) => {
   let appointment = req.appointment;
   appointment = _.assignIn(appointment, req.body);
-  appointment.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Update Appointment failed!',
-      });
-    }
-    res.json({
-      message: 'Update appointment successfully',
+  try {
+    const currentAppointment = await appointment.save();
+    const data = await currentAppointment.populate([
+      { path: "service_id" },
+      { path: "customer_id" },
+      { path: "staff_id", populate: { path: "user_id" } },
+    ]);
+    return res.json({
+      message: "Update appointment successfully",
       data,
     });
-  });
+  } catch (error) {
+    return res.status(400).json({
+      error: "Update Appointment failed!",
+    });
+  }
 };
 
 export const listAppointmentRelated = (req, res) => {
@@ -92,7 +102,7 @@ export const listAppointmentRelated = (req, res) => {
   }).exec((err, data) => {
     if (err) {
       res.status(400).json({
-        error: 'Appointment not found!',
+        error: "Appointment not found!",
       });
     }
     res.json({ data });
