cd55f20f86d1641ac0b61f14c03e1d08202e4073
Trần Quang <songoku2192@gmail.com>
2022-03-22 21:21:07 +0700

    update socket for noti change staff

--- controllers/socket.js
+++ controllers/socket.js
@@ -3,14 +3,14 @@ import {
   listNotificationStaff,
   removeNotificationStaff,
   listNotifications,
-} from "../controllers/notificationStaffController";
+} from '../controllers/notificationStaffController';
 
 export const notification = (io) => {
-  io.on("connection", (socket) => {
-    socket.on("disconnect", () => {});
-    socket.on("notifications-service", (room, id) => {
+  io.on('connection', (socket) => {
+    socket.on('disconnect', () => {});
+    socket.on('notifications-service', (room, id) => {
       switch (room) {
-        case "create-notifications-service":
+        case 'create-notifications-service':
           createNotificationStaff(id).then((data) => {
             listNotificationStaff(data.staff_id).then((noti) => {
               socket.broadcast.emit(
@@ -23,29 +23,43 @@ export const notification = (io) => {
             });
           });
           break;
-        case "list-notifications-service":
+        case 'list-notifications-service':
           listNotificationStaff(id.id).then((noti) => {
             socket.emit(`send-notification-service-${id.id}`, noti);
           });
           break;
-        case "notification-admin-room":
+        case 'notification-admin-room':
           listNotifications().then((noti) => {
             socket.emit(`send-notification-admin`, noti);
           });
           break;
-        case "notification-create-order":
+        case 'notification-create-order':
           break;
-        case "list-notifications-order":
+        case 'list-notifications-order':
+          break;
+        case 'notification-change-permission':
+          createNotificationStaff(id)
+            .then((data) => {
+              listNotificationStaff(data.staff_id).then((noti) => {
+                socket.broadcast.emit(
+                  `send-notification-permission-${data.staff_id}`,
+                  noti
+                );
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
           break;
         default:
           break;
       }
     });
 
-    socket.on("remove-notification", (id) => {
+    socket.on('remove-notification', (id) => {
       removeNotificationStaff(id).then((data) => {
         listNotificationStaff(data.staff_id).then((noti) => {
-          socket.broadcast.emit("send-message", noti);
+          socket.broadcast.emit('send-message', noti);
         });
       });
     });
