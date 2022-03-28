4e39a9af50c2d7246c7db4bc64b1c84113f75bf2
Trần Quang <songoku2192@gmail.com>
2022-03-28 21:46:52 +0700

    ud fix bug product

--- models/productModel.js
+++ models/productModel.js
@@ -1,52 +1,56 @@
 import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Schema;
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     product_name: {
-        type: String,
-        required: true,
-        maxLength: 100,
+      type: String,
+      required: true,
+      maxLength: 100,
     },
     product_amount: {
-        type: Number,
-        required: true,
-        maxLength: 21,
+      type: Number,
+      required: true,
+      maxLength: 21,
     },
     product_price: {
-        type: Number,
-        required: true,
-        maxLength: 21,
+      type: Number,
+      required: true,
+      maxLength: 21,
     },
     product_sale: {
-        type: Number,
-        required: true,
-        default: 0,
-        maxLength: 21,
+      type: Number,
+      required: true,
+      default: 0,
+      maxLength: 21,
     },
     product_star: {
-        type: Number,
-        required: true,
-        default: 0,
-        max: 5,
+      type: Number,
+      required: true,
+      default: 0,
+      max: 5,
     },
     product_description: {
-        type: String,
-        required: true,
-        maxLength: 300,
+      type: String,
+      required: true,
+      maxLength: 300,
+      default: 'Đang cập nhật',
     },
     product_content: {
-        type: String,
+      type: String,
     },
     product_album: {
-        type: Array,
-        required: true,
+      type: Array,
+      required: true,
     },
     category_id: {
-        type: ObjectId,
-        ref: 'Category',
-        required: true,
+      type: ObjectId,
+      ref: 'Category',
+      required: true,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 module.exports = mongoose.model('Product', ProductSchema);
