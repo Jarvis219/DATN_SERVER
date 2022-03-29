80755f13e402d307fddffb1aa05fead6e66cf8be
phih <phih.miichisoft@gmail.com>
2022-03-29 09:44:39 +0700

    create model-controller-routes api order

---
+++ routes/order.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { list, create, orderById, read, remove, update, orderByUser } from '../controllers/orderControllers';
+import { userId } from '../controllers/userControllers';
+
+const router = express.Router();
+
+//Danh sách đơn hàng 
+router.get('/list-order', list);
+//Danh sách đơn hàng theo user
+router.get('/list-order/:userId', orderByUser);
+//Thêm đơn hàng 
+router.post('/create-order', create);
+//Chi tiêt đơn hàng
+router.get('/read-order/:orderId', read);
+//Xoá đơn hàng 
+router.delete('/remove-order/:orderId', remove);
+//Cập nhật trạng thái đơn hàng
+router.put('/update-order/:orderId', update);
+//Lấy param
+router.param('orderId', orderById);
+router.param('userId', userId);
+
+module.exports = router;
