0ae624e8c2df41b33a3f36a0892c53d725e8ccaa
Trần Quang <songoku2192@gmail.com>
2022-03-30 13:57:43 +0700

    ud

--- app.js
+++ app.js
@@ -1,93 +1,93 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { notification } from './controllers/socket';
-import { scheduleJob, RecurrenceRule } from 'node-schedule-tz';
-import { handleUpdateWorkdayHistory } from './controllers/handleUpdateWorkdayHistory';
-const { instrument } = require('@socket.io/admin-ui');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import { notification } from "./controllers/socket";
+import { scheduleJob, RecurrenceRule } from "node-schedule-tz";
+import { handleUpdateWorkdayHistory } from "./controllers/handleUpdateWorkdayHistory";
+const { instrument } = require("@socket.io/admin-ui");
 
 const app = express();
 dotenv.config();
 
 const rule = new RecurrenceRule();
-rule.hour = 0;
-rule.minute = 1;
-rule.second = 1;
-rule.tz = 'Etc/GMT-7';
+rule.hour = 14;
+rule.minute = 0;
+rule.second = 0;
+rule.tz = "Etc/GMT-7";
 scheduleJob(rule, function () {
-  handleUpdateWorkdayHistory();
+	handleUpdateWorkdayHistory();
 });
 
 // server with socket
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  },
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	},
 });
 instrument(io, {
-  auth: false,
+	auth: false,
 });
 
 // Router
-const authRouters = require('./routes/auth');
-const serviceRouter = require('./routes/service');
-const staffRouter = require('./routes/staff');
-const categoryRouter = require('./routes/category');
-const productRouter = require('./routes/product');
-const salaryRouter = require('./routes/salary');
-const userRouter = require('./routes/user');
-const appointmentRouter = require('./routes/appointment');
-const customerRouter = require('./routes/customer');
-const cartRouter = require('./routes/cart');
-const invoiceRouter = require('./routes/invoice');
-const detailInvoiceRouter = require('./routes/detailInvoice');
-const employeeJobDetail = require('./routes/employeeJobDetail');
-const contactRouter = require('./routes/contact');
-const workdayHistoryRouter = require('./routes/workdayHistory');
+const authRouters = require("./routes/auth");
+const serviceRouter = require("./routes/service");
+const staffRouter = require("./routes/staff");
+const categoryRouter = require("./routes/category");
+const productRouter = require("./routes/product");
+const salaryRouter = require("./routes/salary");
+const userRouter = require("./routes/user");
+const appointmentRouter = require("./routes/appointment");
+const customerRouter = require("./routes/customer");
+const cartRouter = require("./routes/cart");
+const invoiceRouter = require("./routes/invoice");
+const detailInvoiceRouter = require("./routes/detailInvoice");
+const employeeJobDetail = require("./routes/employeeJobDetail");
+const contactRouter = require("./routes/contact");
+const workdayHistoryRouter = require("./routes/workdayHistory");
 
 //db connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB Connected'));
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error', (err) => {
-  console.log(`DB connection error: ${err.message}`);
+mongoose.connection.on("error", (err) => {
+	console.log(`DB connection error: ${err.message}`);
 });
 
 // Middleware
 app.use(express.json());
 app.use(
-  cors({
-    credentials: 'same-origin',
-  })
+	cors({
+		credentials: "same-origin",
+	})
 );
 
 notification(io);
 
-app.use('/api', authRouters);
-app.use('/api', serviceRouter);
-app.use('/api', staffRouter);
-app.use('/api', categoryRouter);
-app.use('/api', productRouter);
-app.use('/api', salaryRouter);
-app.use('/api', userRouter);
-app.use('/api', cartRouter);
-app.use('/api', contactRouter);
-app.use('/api', workdayHistoryRouter);
+app.use("/api", authRouters);
+app.use("/api", serviceRouter);
+app.use("/api", staffRouter);
+app.use("/api", categoryRouter);
+app.use("/api", productRouter);
+app.use("/api", salaryRouter);
+app.use("/api", userRouter);
+app.use("/api", cartRouter);
+app.use("/api", contactRouter);
+app.use("/api", workdayHistoryRouter);
 // api đặt lịch
-app.use('/api', appointmentRouter);
-app.use('/api', customerRouter);
-app.use('/api', invoiceRouter);
-app.use('/api', detailInvoiceRouter);
-app.use('/api', employeeJobDetail);
+app.use("/api", appointmentRouter);
+app.use("/api", customerRouter);
+app.use("/api", invoiceRouter);
+app.use("/api", detailInvoiceRouter);
+app.use("/api", employeeJobDetail);
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.log(`Server is running on port : ${port}`);
+	console.log(`Server is running on port : ${port}`);
 });
