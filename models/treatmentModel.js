f89c0c049bf4bf6eba861e9f9b7e7d7bc59ef21e
Trần Quang <songoku2192@gmail.com>
2022-04-09 13:18:09 +0700

    update code status treatment

--- models/treatmentModel.js
+++ models/treatmentModel.js
@@ -1,21 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const treatmentSchema = new mongoose.Schema(
-	{
-		number_of_treatments: {
-			type: Number,
-			required: true,
-			default: 1,
-		},
-		treatment_name: {
-			type: String,
-			required: true,
-			maxlength: 50,
-			unique: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    number_of_treatments: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    treatment_name: {
+      type: String,
+      required: true,
+      maxlength: 50,
+      unique: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Treatment", treatmentSchema);
+module.exports = mongoose.model('Treatment', treatmentSchema);
