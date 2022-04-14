ffc1105ccf98e204f5c562a3c50853dd37a2b91f
phihph12890 <65588792+phihph12890@users.noreply.github.com>
2022-04-14 23:29:12 +0700

    update

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,15 +1,15 @@
-import Order from '../models/orderModel';
+import Order from "../models/orderModel";
 
 export const list = (req, res) => {
   Order.find()
     .sort({
       updatedAt: -1,
     })
-    .populate('user', '_id name email')
+    .populate("user", "_id name email")
     .exec((err, data) => {
       if (err) {
         res.status(400).json({
-          error: 'Order not found',
+          error: "Order not found",
         });
       }
       res.json(data);
@@ -20,7 +20,7 @@ export const orderByUser = (req, res) => {
   Order.find({ user: req.user._id }, (err, orders) => {
     if (err) {
       res.status(400).json({
-        error: 'Orders not found',
+        error: "Orders not found",
       });
     }
     res.json(orders);
@@ -33,7 +33,7 @@ export const create = (req, res) => {
     if (err) {
       console.log(err);
       return res.status(400).json({
-        error: 'Add order failed',
+        error: "Add order failed",
       });
     }
     res.json(data);
@@ -44,7 +44,7 @@ export const orderById = (req, res, next, id) => {
   Order.findById(id).exec((err, order) => {
     if (err || !order) {
       res.status(404).json({
-        error: 'Không tìm thấy đơn hàng!',
+        error: "Không tìm thấy đơn hàng!",
       });
     }
     req.order = order;
@@ -61,30 +61,30 @@ export const remove = (req, res) => {
   order.remove((err, data) => {
     if (err) {
       return res.status(400).json({
-        error: 'Không xoá được đơn hàng!',
+        error: "Không xoá được đơn hàng!",
       });
     }
     res.json({
       data,
-      message: 'Xoá đơn hàng thành công',
+      message: "Xoá đơn hàng thành công",
     });
   });
 };
 
-export const update = (req, res) => {
+export const update = async (req, res) => {
   let order = req.order;
   order.status = req.body.status;
 
   try {
-    const dataOrder = await order.save()
-    const data = await dataOrder.populate('user', '_id name email')
+    const dataOrder = await order.save();
+    const data = await dataOrder.populate("user", "_id name email");
     return res.status(200).json({
-      message: 'Update successfully!',
+      message: "Update successfully!",
       data,
     });
   } catch (error) {
     return res.status(400).json({
-      error: 'Update failed!',
+      error: "Update failed!",
     });
   }
 };
