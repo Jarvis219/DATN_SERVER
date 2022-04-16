162a26788c50c1cae1941178c1dde1e7e5f48a0f
Trần Quang <songoku2192@gmail.com>
2022-04-16 09:35:48 +0700

    update code

--- models/serviceModel.js
+++ models/serviceModel.js
@@ -1,58 +1,54 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Schema;
 const ServiceSchema = new mongoose.Schema(
-	{
-		service_name: {
-			type: String,
-			required: true,
-			maxLength: 100,
-		},
-		service_price: {
-			type: Number,
-			required: true,
-			maxLength: 21,
-		},
-		service_sale: {
-			type: Number,
-			required: true,
-			default: 0,
-			maxLength: 21,
-		},
-		service_star: {
-			type: Number,
-			required: true,
-			default: 0,
-			maxLength: 5,
-		},
-		service_description: {
-			type: String,
-			required: true,
-			maxLength: 300,
-		},
-		service_content: {
-			type: String,
-		},
-		service_album: {
-			type: Array,
-			required: true,
-		},
-		category_id: {
-			type: ObjectId,
-			ref: "Category",
-			required: true,
-		},
-		service_time: {
-			type: Number,
-			required: true,
-		},
-		brand_id: {
-			type: ObjectId,
-			ref: "Brand",
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    service_name: {
+      type: String,
+      required: true,
+      maxLength: 100,
+    },
+    service_price: {
+      type: Number,
+      required: true,
+      maxLength: 21,
+    },
+    service_sale: {
+      type: Number,
+      required: true,
+      default: 0,
+      maxLength: 21,
+    },
+    service_star: {
+      type: Number,
+      required: true,
+      default: 0,
+      maxLength: 5,
+    },
+    service_description: {
+      type: String,
+      required: true,
+      maxLength: 300,
+    },
+    service_content: {
+      type: String,
+    },
+    service_album: {
+      type: Array,
+      required: true,
+    },
+    category_id: {
+      type: ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    service_time: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model("Service", ServiceSchema);
+module.exports = mongoose.model('Service', ServiceSchema);
