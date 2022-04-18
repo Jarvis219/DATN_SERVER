67fe943bdb8a2669103206b48dc182d261d7f6ed
Tran Quang <kysumuoiba@teneocto.io>
2022-04-18 16:05:03 +0700

    update code

--- models/appointmentTreatmentDetailModel.js
+++ models/appointmentTreatmentDetailModel.js
@@ -1,45 +1,45 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
 const appointmentTreatmentDetail = new mongoose.Schema(
-  {
-    status: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    date: {
-      type: String,
-      required: true,
-    },
-    staff_id: {
-      type: ObjectId,
-      required: true,
-      ref: 'Staff',
-    },
-    appointment_treatment_id: {
-      type: ObjectId,
-      required: true,
-      ref: 'AppointmentTreatment',
-    },
-    note: {
-      type: String,
-    },
-    number_treatment: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    notification: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		status: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		date: {
+			type: String,
+			required: true,
+		},
+		staff_id: {
+			type: ObjectId,
+			required: true,
+			ref: "Staff",
+		},
+		appointment_treatment_id: {
+			type: ObjectId,
+			required: true,
+			ref: "AppointmentTreatment",
+		},
+		note: {
+			type: String,
+		},
+		number_treatment: {
+			type: Number,
+			required: true,
+			default: 1,
+		},
+		notification: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 module.exports = mongoose.model(
-  'AppointmentTreatmentDetail',
-  appointmentTreatmentDetail
+	"AppointmentTreatmentDetail",
+	appointmentTreatmentDetail
 );
